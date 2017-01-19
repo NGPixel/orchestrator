6f26f50ad264dc89e40d383dac69a18e03ae59e6
NGPixel <github@ngpixel.com>
2017-01-19 00:20:27 -0500

    Devices Lights scan

--- devices/philips-hue.js
+++ devices/philips-hue.js
@@ -1,29 +1,31 @@
 'use strict'
 
-// const Promise = require('bluebird')
+const Promise = require('bluebird')
 const huejay = require('huejay')
 const _ = require('lodash')
 
 /**
  * Philips Hue devices module
- *
- * @return     {Object}  Philips Hue devices instance
  */
 module.exports = {
 
+  brand: 'Philips Hue', // REQUIRED
+
   /**
-   * Refresh devices
+   * Scan for bridges on the network
    *
-   * @return     {Object}  DB instance
+   * @return     {Promise<Object[], Error>}  List of found bridges
    */
   scanBridges () {
+    let self = this
+
     return huejay.discover()
     .then(bridges => {
       return _.map(bridges, b => {
         return {
           ui: b.id,
           name: 'New Bridge (' + b.id + ')',
-          brand: 'Philips Hue',
+          brand: self.brand,
           model: 'Unknown',
           state: 'pending',
           ipAddress: b.ipAddress,
@@ -32,6 +34,62 @@ module.exports = {
           meta: {},
           isSetup: false
         }
+      })
+    })
+  },
+
+  /**
+   * Setup new bridge for use in Orchestrator
+   *
+   * @param      {Object}  bridge  The new bridge to setup
+   * @return {Promise<Object>} The configured bridge
+   */
+  setupBridge (bridge) {
+    // todo
+  },
+
+  /**
+   * Scans all registered lights in bridges
+   *
+   * @param      {Array<Object>}  bridges  The bridges to scan
+   * @return     {Promise<Array<Object>, Error>}  List of lights
+   */
+  scanLights (bridges) {
+    let self = this
+
+    return Promise.map(bridges, br => {
+      let client = new huejay.Client({
+        host: br.ipAddress,
+        username: br.auth
+      })
+
+      return client.bridge.ping().then(() => {
+        return client.bridge.isAuthenticated()
+      }).then(() => {
+        return client.lights.getAll().then(lights => {
+          return _.map(lights, lt => {
+            return {
+              state: lt.on,
+              colorMode: lt.colorMode,
+              brightness: lt.brightness,
+              hue: lt.hue,
+              sat: lt.saturation,
+              xy: lt.xy,
+              ct: lt.colorTemp,
+              alert: lt.alert,
+              effect: lt.effect,
+              uid: lt.id,
+              name: lt.name,
+              brand: self.brand,
+              model: lt.modelId,
+              meta: {
+                swversion: lt.softwareVersion,
+                type: lt.model.type,
+                uniqueid: lt.uniqueId
+              }
+            }
+          })
+        })
       })
     })
   }
--- libs/devices.js
+++ libs/devices.js
@@ -7,18 +7,19 @@ const _ = require('lodash')
 
 /**
  * Devices module
- *
- * @return     {Object}  Devices instance
  */
 module.exports = {
 
   moduleList: {},
 
+  /**
+   * Initialize the Devices class
+   *
+   * @return     {Object}  Self
+   */
   init () {
     let self = this
     let devicesPath = path.resolve(ROOTPATH, 'devices')
-
-    // Load device modules
 
     fs
     .readdirSync(devicesPath)
@@ -34,35 +35,9 @@ module.exports = {
   },
 
   /**
-   * Refresh devices
+   * Scans bridges across all modules
    *
-   * @return     {Object}  DB instance
-   */
-  scanLights () {
-    let self = this
-
-    return Promise.map(self.moduleList, mod => {
-      if (_.has(mod, 'scanBridges')) {
-        mod.scanBridges().then(brs => {
-          if (_.isArray(brs) && brs.length > 0) {
-            return Promise.map(brs, br => {
-              return db.Bridge.filter({ uid: br.uid, brand: br.brand }).count().run().then(c => {
-                if (c < 1) {
-                  let newBr = new db.Bridge(br)
-                  return newBr.insert(br)
-                } else {
-                  return Promise.resolve(true)
-                }
-              })
-            })
-          }
-        })
-      }
-    })
-  },
-
-  /**
-   * Scans bridges.
+   * @return {Promise<Boolean, Error>} Promise of the operation
    */
   scanBridges () {
     let self = this
@@ -75,7 +50,7 @@ module.exports = {
               return db.Bridge.filter({ uid: br.uid, brand: br.brand }).count().run().then(c => {
                 if (c < 1) {
                   let newBr = new db.Bridge(br)
-                  return newBr.insert(br)
+                  return newBr.save(br)
                 } else {
                   return Promise.resolve(true)
                 }
@@ -83,6 +58,43 @@ module.exports = {
             })
           }
         })
+      }
+    })
+  },
+
+  /**
+   * Refresh light devices from all bridges
+   *
+   * @return     {Promise<Boolean, Error>}  Promise of the operation
+   */
+  scanLights () {
+    let self = this
+
+    return Promise.map(self.moduleList, mod => {
+      if (_.has(mod, 'scanLights')) {
+        return db.Bridge.filter({ brand: mod.brand }).run().then(bridges => {
+          if (_.isArray(bridges) && bridges.length > 0) {
+            mod.scanLights(bridges).then(lights => {
+              if (_.isArray(lights) && lights.length > 0) {
+                return Promise.map(lights, lt => {
+                  return db.Device.filter({ uid: lt.uid, brand: lt.brand }).count().run().then(c => {
+                    if (c < 1) {
+                      // todo
+                      let newLt = new db.Light(lt)
+                      return newLt.save(lt)
+                    } else {
+                      return Promise.resolve(true)
+                    }
+                  })
+                })
+              }
+            })
+          } else {
+            return Promise.resolve(true)
+          }
+        })
+      } else {
+        return Promise.resolve(true)
       }
     })
   }
